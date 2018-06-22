02b95961497f8125dab73bafe7b287de5dd5749e
Francois Budin <francois.budin@kitware.com>
2018-06-22 18:21:21 -0400

    WIP Add ADIOS2 as a dependency for IOSS
    ADIOS2 is an I/O framework that is developed as part of the U.S.
    Department of Energy Exascale Computing Program.

---
+++ packages/seacas/libraries/ioss/src/adios/Ioad_DatabaseIO.h
@@ -0,0 +1,143 @@
+// Copyright(C) 1999-2010 National Technology & Engineering Solutions
+// of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
+// NTESS, the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of NTESS nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// -*- Mode: c++ -*-
+#ifndef IOSS_Ioad_DatabaseIO_h
+#define IOSS_Ioad_DatabaseIO_h
+
+#include <Ioss_DBUsage.h>
+#include <Ioss_DatabaseIO.h>
+
+
+
+namespace Ioss {
+  class GroupingEntity;
+  class Region;
+  class EntityBlock;
+  class NodeBlock;
+  class SideBlock;
+  class ElementBlock;
+  class NodeSet;
+  class SideSet;
+  class CommSet;
+}
+
+namespace adios2 {
+   class IO;
+   class ADIOS;
+   class Engine;
+}
+
+/** \brief A namespace for the adios database format.
+ */
+namespace Ioad {
+
+  class DatabaseIO : public Ioss::DatabaseIO
+  {
+  public:
+    DatabaseIO(Ioss::Region *region, const std::string &filename, Ioss::DatabaseUsage db_usage,
+               MPI_Comm communicator, const Ioss::PropertyManager &properties_x);
+    ~DatabaseIO();
+    DatabaseIO(const DatabaseIO &from) = delete;
+    DatabaseIO &operator=(const DatabaseIO &from) = delete;
+
+    bool begin__(Ioss::State state) override;
+    bool end__(Ioss::State state) override;
+
+    unsigned int entity_field_support () const {return 0;}
+
+  private:
+    int64_t get_field_internal(const Ioss::Region *reg, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::NodeBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::EdgeBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::FaceBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::ElementBlock *eb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::StructuredBlock *sb, const Ioss::Field &field,
+                               void *data, size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::SideBlock *fb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::NodeSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::EdgeSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::FaceSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::ElementSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::SideSet *fs, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t get_field_internal(const Ioss::CommSet *cs, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+
+    int64_t put_field_internal(const Ioss::Region *reg, const Ioss::Field &field, void *data,
+                               size_t data_size) const override;
+    int64_t put_field_internal(const Ioss::NodeBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::EdgeBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::FaceBlock *nb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::ElementBlock *eb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::SideBlock *fb, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::NodeSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::EdgeSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::FaceSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::ElementSet *ns, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::SideSet *fs, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::CommSet *cs, const Ioss::Field &field, void *data,
+                               size_t data_size) const override {return 0;}
+    int64_t put_field_internal(const Ioss::StructuredBlock *sb, const Ioss::Field &field,
+                               void *data, size_t data_size) const override {return 0;}
+    void read_meta_data__ () override {}
+    void write_meta_data();
+
+    int64_t element_global_to_local__(int64_t global) const {return 0;}
+    int64_t node_global_to_local__(int64_t global, bool must_exist) const {return 0;}
+    
+    adios2::IO *bpio;
+    adios2::ADIOS *ad;
+    adios2::Engine *bpWriter;
+  };
+}
+#endif
